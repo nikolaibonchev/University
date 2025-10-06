98342beda61c8cc447c67692678875b6669e3a0a
Nikolay Bonchev <nikieew@gmail.com>
2025-10-06 08:20:40 +0300

    LAB 2

---
+++ PP/folder_proj/lab2.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char *argv[], char *envp[]) {
+    int i;
+
+    printf("Command-line arguments (argc = %d):\n", argc);
+    for (i = 0; i < argc; ++i) {
+        printf("argv[%d] = %s\n", i, argv[i]);
+    }
+
+    printf("\nEnvironment variables:\n");
+    for (i = 0; envp[i] != NULL; ++i) {
+        printf("%s\n", envp[i]);
+    }
+
+    return 0;
+}
