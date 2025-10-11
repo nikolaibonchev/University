3b14ce530f67429ae6fe0c3c9e165c2c13349d01
Nikolay Bonchev <nikieew@gmail.com>
2025-10-11 16:16:03 +0300

    Done Lab3 to condition 3)

--- PP/folder_proj/lab3.c
+++ PP/folder_proj/lab3.c
@@ -1,18 +1,41 @@
 #include <stdio.h>
+#include <string.h>
+
 
 int main(int argc, char *argv[], char *envp[]) {
-	int i, j;
+	int i,found;
 
-	for (i = 1; i < argc; i++) {
-         for (j = 0; j < argc; j++) {
-	  if(argv[i] == envp[j]){
-	   printf("\n%s\n", envp[j]);
-	  } else {
-	   printf("No enviroments found");
-	  }
-	 }
+	char buffer[512], argCopy[512];
+	char *name, *value, *context, *token;
+	
+	if(argv[1] == NULL){
+	 printf("Please do mind that for this program to work you'll need command-line arguments\n");
+	};
+
+	strcpy(argCopy, argv[1]);
+	token = strtok(argv[1], ",");	
+
+	while(token != NULL){
+	 found = 0;
+	
+	 for (i = 0; envp[i] != NULL; i++) {
+	  strcpy(buffer, envp[i]);
+	  name = strtok_s(buffer, "=", &context);
+	  value = strtok_s(NULL, "", &context);
+
+	  if(strcmp(token, name) == 0){
+	   printf("ENV Name: %s\nENV Value: %s\n\n", name, value);
+	   found = 1;
+	   break;
+	  };
+    	 };
+
+	if(!found){
+	 printf("Environment variable '%s' not found.\n\n", token);
 	}
-    
+	 
+	 token = strtok(NULL, ",");
+	};
 
     return 0;
-}
+};
