3d7e7f76161c8d7ad4c803a62fedbbb82bbe7bfe
Nikolay Bonchev <nikieew@gmail.com>
2026-02-13 08:56:13 +0200

    Begin work on Lab 1.1

--- PS/PE_lab_1_1/PE_lab_Win32CRT_proj/Win32Console_MSVS2022/Win32Console/Win32Console/Win32Console.c
+++ PS/PE_lab_1_1/PE_lab_Win32CRT_proj/Win32Console_MSVS2022/Win32Console/Win32Console/Win32Console.c
@@ -3,9 +3,44 @@
 
 #include "stdafx.h"
 
+FILE* stream;
 
 int _tmain(int argc, _TCHAR* argv[])
 {
+	long l;
+	float fp;
+	char s[81];
+	char c;
+
+	for (int i = 0; i < argc; i++) {
+		errno_t err = fopen_s(&stream, argv[i], "w+");
+
+        if (err)
+            printf_s("The file %s was not opened\n", argv[i]);
+        else
+        {
+            fprintf_s(stream, "%s %ld %f%c", "a-string",
+                65000, 3.14159, 'x');
+            // Set pointer to beginning of file:
+            fseek(stream, 0L, SEEK_SET);
+
+            // Read data back from file:
+            fscanf_s(stream, "%s", s, _countof(s));
+            fscanf_s(stream, "%ld", &l);
+
+            fscanf_s(stream, "%f", &fp);
+            fscanf_s(stream, "%c", &c, 1);
+
+            // Output data read:
+            printf("%s\n", s);
+            printf("%ld\n", l);
+            printf("%f\n", fp);
+            printf("%c\n", c);
+
+            fclose(stream);
+        }
+	}
+
 	return 0;
 }
 
