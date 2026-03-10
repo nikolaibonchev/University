a49cc7f04c79cbd7a006cebaa1187d5a6aab8ee5
Nikolay Bonchev <nikieew@gmail.com>
2026-03-10 13:11:06 +0200

    Lab3.2 on Java ready

--- Java/Lab3.2/src/Main.java
+++ Java/Lab3.2/src/Main.java
@@ -2,6 +2,16 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
+        Student studA = new Student("Ivan", new int[]{4, 5, 6, 3, 4});
+        Student studB = new Student("Pesho", new int[]{3,5,5,6,4});
+        Student studC = new Student("Muncho", new int[]{3,4,3,4,2,2});
 
+        double avgA = studA.avarageGrade();
+        double avgB = studB.avarageGrade();
+        double avgC = studC.avarageGrade();
+
+        System.out.println("The avarage grade of StudA is: " +avgA);
+        System.out.println("The avarage grade of StudB is: " +avgB);
+        System.out.println("The avarage grade of StudC is: " +avgC);
     }
 }
--- Java/Lab3.2/src/Student.java
+++ Java/Lab3.2/src/Student.java
@@ -1,6 +1,20 @@
 public class Student {
     String name;
-    int[] grade;
+    int[] grades;
 
-    Student(String inName, )
+    Student(String inName, int[] inGrades){
+        name = inName;
+        grades = inGrades;
+    }
+
+    double avarageGrade(){
+        int count = 0;
+        double total = 0;
+        for (int grade:grades){
+            count++;
+            total += grade;
+        }
+
+        return total / count;
+    };
 }
