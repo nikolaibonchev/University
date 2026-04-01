b23d30828e73b3202dbe687c147308aeb40cf4ec
Nikolay Bonchev <nikieew@gmail.com>
2026-04-01 15:31:27 +0300

    Lab7 of OOP ready

--- OOP/Lab7/Lab7/Lab7.cpp
+++ OOP/Lab7/Lab7/Lab7.cpp
@@ -41,9 +41,9 @@ void TPoint::setY(int iy) {
 };
 
 class GameUnit {
-private:
-	TPoint* Position;
-	TPoint* Target;
+protected:
+	TPoint Position;
+	TPoint Target;
 	int Condition;
 	int MoveSpeed;
 	int TactMoveSpeed;
@@ -52,14 +52,14 @@ private:
 
 public:
 	GameUnit();
-	GameUnit(TPoint* inPos, TPoint* inTar, int inCond, int inMS, int inAS);
-	void Attack();
-	void Move();
+	GameUnit(TPoint inPos, TPoint inTar, int inCond, int inMS, int inAS);
+	virtual ~GameUnit() {};
+	virtual void Attack();
+	virtual void Move();
+	void PrintPosition();
 };
 
 GameUnit::GameUnit() {
-	Position = nullptr;
-	Target = nullptr;
 	Condition = 0;
 	MoveSpeed = 0;
 	AttackSpeed = 0;
@@ -68,26 +68,54 @@ GameUnit::GameUnit() {
 
 };
 
-GameUnit::GameUnit(TPoint* inPos, TPoint* inTar, int inCond, int inMS, int inAS) {
-	int tarX = inTar->getX();
-	int tarY = inTar->getY();
-
-	if (tarX == -1) inTar->setX(0);
-	if (tarY == -1) inTar->setY(0);
-
+GameUnit::GameUnit(TPoint inPos, TPoint inTar, int inCond, int inMS, int inAS) {
 	Position = inPos;
 	Target = inTar;
 	Condition = inCond;
 	MoveSpeed = inMS;
 	AttackSpeed = inAS;
+	TactMoveSpeed = 5 - MoveSpeed;
+	TactAttackSpeed = 5 - AttackSpeed;
 };
 
 void GameUnit::Attack() {
+	if (Condition <= 0 || AttackSpeed == 0) return;
 
+	TactAttackSpeed--;
+
+	TactAttackSpeed++;
+	if (TactAttackSpeed <= 0) {
+		std::cout << "Unit attacks!\n";
+		TactAttackSpeed = 5 - AttackSpeed;;
+	};
 }
 
 void GameUnit::Move() {
+	if (Condition <= 0 || MoveSpeed == 0) return;
 
+	TactMoveSpeed--;
+
+	if (TactMoveSpeed <= 0) {
+		if (Position.getX() < Target.getX()) {
+			Position.setX(Position.getX() + 1);
+		}
+		else if (Position.getX() > Target.getX()) {
+			Position.setX(Position.getX() - 1);
+		}
+
+		if (Position.getY() < Target.getY()) {
+			Position.setY(Position.getY() + 1);
+		}
+		else if (Position.getY() > Target.getY()) {
+			Position.setY(Position.getY() - 1);
+		}
+
+		TactMoveSpeed = 5 - MoveSpeed;
+	}
+}
+
+void GameUnit::PrintPosition() {
+	std::cout << "Unit position: (" << Position.getX() << ", " << Position.getY() << ")\n";
 }
 
 class Knight: public GameUnit {
@@ -97,7 +125,8 @@ private:
 
 public:
 	Knight();
-	Knight(int inSword, int inArmor);
+	Knight(TPoint inPos, TPoint inTar, int inCond, int inMS, int inAS, int inSword, int inArmor);
+	void Attack() override;
 };
 
 Knight::Knight() {
@@ -105,10 +134,26 @@ Knight::Knight() {
 	Armor = 0;
 };
 
-Knight::Knight(int inSword, int inArmor) {
+Knight::Knight(TPoint inPos, TPoint inTar, int inCond, int inMS, int inAS, int inSword, int inArmor) {
+	Position = inPos;
+	Target = inTar;
+	Condition = inCond;
+	MoveSpeed = inMS;
+	AttackSpeed = inAS;
+
 	Sword = inSword;
 	Armor = inArmor;
 };
+
+void Knight::Attack() {
+	if (Condition <= 0 || AttackSpeed == 0 ) return;
+
+	TactAttackSpeed--;
+	if (TactAttackSpeed <= 0) {
+		std::cout << "Knight swings sword (Damage: " << Sword << ")!\n";
+		TactAttackSpeed = 5 - AttackSpeed;
+	};
+}
 
 class Monster: public GameUnit {
 private:
@@ -117,7 +162,8 @@ private:
 
 public:
 	Monster();
-	Monster(int inNails, int inTeeth);
+	Monster(TPoint inPos, TPoint inTar, int inCond, int inMS, int inAS, int inNails, int inTeeth);
+	void Attack() override;
 };
 
 Monster::Monster() {
@@ -125,34 +171,79 @@ Monster::Monster() {
 	Teeth = 0;
 };
 
-Monster::Monster(int inNails, int inTeeth) {
+Monster::Monster(TPoint inPos, TPoint inTar, int inCond, int inMS, int inAS, int inNails, int inTeeth){
+	Position = inPos;
+	Target = inTar;
+	Condition = inCond;
+	MoveSpeed = inMS;
+	AttackSpeed = inAS;
+
 	Nails = inNails;
 	Teeth = inTeeth;
+}
+
+void Monster::Attack() {
+	if (Condition <= 0 || AttackSpeed == 0) return;
+
+	TactAttackSpeed--;
+	if (TactAttackSpeed <= 0) {
+		std::cout << "Monster bites with teeth (Damage: " << Teeth << ")!\n";
+		TactAttackSpeed = 5 - AttackSpeed;
+	};
 }
 
 class GameEngine {
 private:
 	GameUnit* array[100];
+	int unitCount;
 
 public:
+	GameEngine();
 	void addElement(GameUnit* unit);
-	void Action(std::string type, GameUnit* unit);
+	void Action();
 };
+
+GameEngine::GameEngine() {
+	unitCount = 0;
+}
 
 void GameEngine::addElement(GameUnit* unit) {
-
-};
-
-void GameEngine::Action(std::string type, GameUnit* unit) {
-	if (type == "attack") {
-		unit->Attack();
+	if (unitCount < 100) {
+		array[unitCount] = unit;
+		unitCount++;
 	}
 	else {
-		unit->Move();
+		std::cout << "GameEngine is full!\n";
+	}
+};
+
+void GameEngine::Action() {
+	std::cout << "--- Game Tick ---\n";
+	for (int i = 0; i < unitCount; i++) {
+		array[i]->Attack();
+		array[i]->Move();
 	}
 };
 
 int main()
 {
+	GameEngine engine;
 
+	TPoint knightStart(0, 0), knightTarget(5, 5);
+	Knight* k1 = new Knight(knightStart, knightTarget, 100, 3, 2, 8, 10);
+
+	TPoint monsterStart(10, 10), monsterTarget(-1, -1);
+	Monster* m1 = new Monster(monsterStart, monsterTarget, 100, 4, 4, 5, 7);
+
+	engine.addElement(k1);
+	engine.addElement(m1);
+
+	for (int i = 0; i < 4; i++) {
+		engine.Action();
+	}
+
+	delete k1;
+	delete m1;
+
+	return 0;
 };
