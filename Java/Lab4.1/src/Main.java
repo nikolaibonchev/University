83b8863e23e5f6a78e2600a2776f5d48de6cc2c2
Nikolay Bonchev <nikieew@gmail.com>
2026-04-06 21:40:52 +0300

    Work on Java Lab4.1

--- Java/Lab4.1/src/Main.java
+++ Java/Lab4.1/src/Main.java
@@ -2,14 +2,13 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Employer emp = new Employer("Gosho", 1000);
+        TeamLeader emp2 = new TeamLeader("Pesho", 2000, 500);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
+        emp1Salary = emp.getYearSalary();
+        emp2Salary = emp2.getYearSalary();
+
+        System.out.println(emp1Salary);
+        System.out.println(emp2Salary);
     }
 }
